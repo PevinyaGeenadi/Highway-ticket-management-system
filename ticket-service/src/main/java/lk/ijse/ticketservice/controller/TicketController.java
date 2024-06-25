299eb7dd67d968d6a402c3aa148bf1065afad7a3
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-06-25 19:26:32 +0530

    Adding RestController and RequestMapping annotations to TicketController

--- ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
@@ -1,4 +1,8 @@
 package lk.ijse.ticketservice.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/ticket")
 public class TicketController {
 }
