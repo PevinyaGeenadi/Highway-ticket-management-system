802949b7fd0e62217b5e4e02a192f989463ffb3b
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-06-25 19:32:37 +0530

    Adding GET endpoint to TicketController

--- ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
@@ -5,4 +5,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/ticket")
 public class TicketController {
+    @GetMapping
+    public String get(){
+        return "ticketservice get method invoked";
+    }
+
 }
