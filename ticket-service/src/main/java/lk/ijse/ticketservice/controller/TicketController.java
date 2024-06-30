5f6a3a960bfd2553123f8a0db0a41afbe5807a89
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-01 01:36:22 +0530

    checkTicket endpoint to TicketController

--- ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/ticket")
 public class TicketController {
-    @GetMapping
-    public String get(){
-        return "ticketservice get method invoked";
+    @GetMapping("/checkTicket")
+    public String userCheck(){
+        return "Ticket Check Successful!";
     }
 
 }
