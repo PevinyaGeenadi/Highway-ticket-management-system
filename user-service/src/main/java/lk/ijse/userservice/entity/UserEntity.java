56801ee9731f6b874df7f4018bb8f74b0837d5d8
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-02 12:23:36 +0530

    update

--- user-service/src/main/java/lk/ijse/userservice/entity/UserEntity.java
+++ user-service/src/main/java/lk/ijse/userservice/entity/UserEntity.java
@@ -1,4 +1,23 @@
 package lk.ijse.userservice.entity;
 
-public class UserEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "user")
+@Entity
+public class UserEntity implements SuperEntity{
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
 }
