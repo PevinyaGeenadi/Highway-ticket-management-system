c9e35c68d460d8f868f0082986a05431651d78a9
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-02 13:01:05 +0530

    added UserServiceIMPL

---
+++ user-service/src/main/java/lk/ijse/userservice/service/impl/UserServiceIMPL.java
@@ -0,0 +1,41 @@
+package lk.ijse.userservice.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.userservice.dao.UserRepo;
+import lk.ijse.userservice.dto.UserDTO;
+import lk.ijse.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceIMPL implements UserService {
+    private final UserRepo userRepo;
+    @Override
+    public UserDTO saveUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public UserDTO getSelectedUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public void updateUser(String userId, UserDTO userDTO) {
+
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+}
