a28d38d39f499207086a62acde8a2fb6ebab4f7e
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-02 12:14:33 +0530

    update

---
+++ user-service/src/main/java/lk/ijse/userservice/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.userservice.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    private String id;
+    private String firstName;
+    private String lastName;
+
+}
