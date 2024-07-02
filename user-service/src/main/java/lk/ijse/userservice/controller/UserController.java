8a211ce391ff29f91e774f4886095839f2c996b7
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-02 23:57:17 +0530

    Adding POST endpoint for saving user details

--- user-service/src/main/java/lk/ijse/userservice/controller/UserController.java
+++ user-service/src/main/java/lk/ijse/userservice/controller/UserController.java
@@ -1,16 +1,42 @@
 package lk.ijse.userservice.controller;
+import lk.ijse.userservice.dto.UserDTO;
+import lk.ijse.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/health")
     public String healthTest() {
         return "User Service is up and running";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveUser(@Validated @RequestBody UserDTO userDTO, BindingResult bindingResult) {
+        logger.info("Save User Details");
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+        }
+        try {
+            UserDTO savedUser = userService.saveUser(userDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        } catch (Exception exception) {
+            logger.error("Error saving User: ", exception);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Internal server error: | User saved Unsuccessfully.Please try again later.\nMore Details\n" + exception);
+        }
     }
 }
