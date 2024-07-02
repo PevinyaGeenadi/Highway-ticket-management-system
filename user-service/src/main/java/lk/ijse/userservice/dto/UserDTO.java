cd1d8795bceb40582e4f352592e67c0c14d47230
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-03 00:07:47 +0530

    Adding validation annotations to UserDTO

--- user-service/src/main/java/lk/ijse/userservice/dto/UserDTO.java
+++ user-service/src/main/java/lk/ijse/userservice/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.userservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @AllArgsConstructor
@@ -7,9 +8,13 @@ import lombok.*;
 @Data
 public class UserDTO {
     private String id;
+    @NotBlank(message = "First Name is must")
     private String firstName;
+    @NotBlank(message = "Last Name is must")
     private String lastName;
+    @NotBlank(message = "Email is must")
     private String email;
+    @NotBlank(message = "Password is must")
     private String password;
 
 }
