213dda745d58afab288aeb27dde5e0899a55544e
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-02 12:43:44 +0530

    Implementing CRUD operations in UserService

---
+++ user-service/src/main/java/lk/ijse/userservice/service/UserService.java
@@ -0,0 +1,13 @@
+package lk.ijse.userservice.service;
+
+import lk.ijse.userservice.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+    UserDTO saveUser(UserDTO userDTO);          // Create
+    UserDTO getSelectedUser(String userId);     // Read (details of a specific user based on their ID
+    List<UserDTO> getAllUser();                 // Read
+    void updateUser(String userId, UserDTO userDTO); // Update
+    void deleteUser(String userId);             // Delete
+}
