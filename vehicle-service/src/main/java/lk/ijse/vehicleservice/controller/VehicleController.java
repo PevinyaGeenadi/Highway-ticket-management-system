f51d03d4eef50aca077c5c62d0506621cbd1d498
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-03 09:23:36 +0530

    Adding health check to VehicleController

--- vehicle-service/src/main/java/lk/ijse/vehicleservice/controller/VehicleController.java
+++ vehicle-service/src/main/java/lk/ijse/vehicleservice/controller/VehicleController.java
@@ -1,4 +1,15 @@
 package lk.ijse.vehicleservice.controller;
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+@RequiredArgsConstructor
 public class VehicleController {
+    @GetMapping("/health")
+    public String healthTest() {
+        return "Vehicle Service is up and running";
+    }
 }
